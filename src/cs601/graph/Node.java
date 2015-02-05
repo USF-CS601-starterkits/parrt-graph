98c50a7e25490d62ae552cca3e9234ffeaaec6b4
Terence Parr <parrt@antlr.org>
2015-02-04 17:24:03 -0800

    update graph project

--- src/cs601/graph/Node.java
+++ src/cs601/graph/Node.java
@@ -1,11 +1,29 @@
 import java.util.Iterator;
 
-public interface Node<ID extends Comparable> extends Comparable {
-	ID getEdge(int i);
+public interface Node<ID extends Comparable> extends Comparable<Node<ID>> {
+	/** Get the ith edge counting from zero. Throw index out of bound exception
+	 *  if i is not within 0..n-1 for n edges
+	 */
+	ID getEdge(int i) throws IndexOutOfBoundsException;
+
+	/** Return the number of edges for this node. */
 	int getEdgeCount();
+
+	/** Added an edge to target node by adding target.getName() to this
+	 *  node's edge list. Do not add an edge with the same name more than once
+	 *  for each node. Do nothing if target is null.
+	 */
 	void addEdge(Node<ID> target);
-	/** Get unique identifier for this node */
+
+	/** Get the unique identifier for this node. You do not have to enforce
+	 *  that this name is unique, but we can assume that the user uses
+	 *  unique names.
+	 */
 	ID getName();
 
+	/** Return an iterator object to that iterates over the edges, if any.
+	 *  If there are no edges, return an iterator that immediately says there
+	 *  are no nodes. This makes it easy to implement many of the algorithms.
+	 */
 	public Iterable<ID> edges();
 }
